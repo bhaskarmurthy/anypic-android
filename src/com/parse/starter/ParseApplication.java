74483ca0e805fefd24784e7cf5e6a2405c58840a
Bhaskar Murthy <bhaskar@murthy.ca>
2013-03-27 00:30:47 -0400

    import from Parse starter project

---
+++ src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,29 @@
+package com.parse.starter;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+import com.parse.ParseUser;
+
+import android.app.Application;
+
+public class ParseApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		
+		ParseACL.setDefaultACL(defaultACL, true);
+	}
+
+}
